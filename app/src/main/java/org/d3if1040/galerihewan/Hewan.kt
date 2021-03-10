66adfc6c4380e352e7d7842b9c58740e31829abc
akhmad_faqih_anshory_fathahilah <faqih49@gmail.com>
2021-03-10 14:34:29 +0700

    Menambahkan gambar pada list

--- app/src/main/java/org/d3if1040/galerihewan/Hewan.kt
+++ app/src/main/java/org/d3if1040/galerihewan/Hewan.kt
@@ -2,5 +2,6 @@ package org.d3if1040.galerihewan
 //dikarenakan saya membuat data class ini secara manual
 data class Hewan(
         val nama: String,
-        val namaLatin: String
+        val namaLatin: String,
+        val imageResId: Int
 )
