cb042492d6a6ab32eed263edaf5fc2c7d095e938
akhmad_faqih_anshory_fathahilah <faqih49@gmail.com>
2021-05-05 00:21:39 +0700

    Menampilkan gambar dari internet

--- app/src/main/java/org/d3if1040/galerihewan/model/Hewan.kt
+++ app/src/main/java/org/d3if1040/galerihewan/model/Hewan.kt
@@ -1,9 +1,7 @@
 package org.d3if1040.galerihewan.model
 
-import org.d3if1040.galerihewan.R
-
 data class Hewan(
         val nama: String,
         val namaLatin: String,
-        val imageResId: Int = R.drawable.angsa
+        val imageId: String
 )
